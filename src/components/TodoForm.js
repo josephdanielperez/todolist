7504d31dcbb17a45de14d5cbcffd93ffd22c35aa
Joseph Perez <hello@josephdanielperez.com>
2021-03-19 16:15:59 -0700

    foundations

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function TodoForm() {
+const [input, setInput] = useState('')
+
     return (
-        <div>
-            
-        </div>
+        <form className='todo-form'>
+            <input type='text' placeholder='Add a todo' value={input} name='text' className='todo-input' />
+            <button className='todo-button'>add todo</button>
+        </form>
     )
 }
 
