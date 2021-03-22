c7b034f8f737d699db5b53e76e14809ab1def21c
Joseph Perez <hello@josephdanielperez.com>
2021-03-22 09:54:21 -0700

    foundations

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,13 +3,24 @@ import { RiCloseCricleLine } from 'react-icons/ri'
 import { TiEdit } from 'react-icons/ti'
 
 import TodoForm from './TodoForm'
-import TodoList from './TodoList'
 
-function Todo({todos, completeTodo}) {
+const Todo = ({ todos, completeTodo, removeTodo, updateTodo }) => {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     })
+
+    const submitUpdate = value => {
+        updateTodo(edit.id, value)
+        setEdit({
+            id: null,
+            value: ''
+        })
+    }
+
+    if (edit.id) {
+        return <TodoForm edit={edit} onSubmit={submitUpdate} />
+    }
 
     return todos.map((todo, index) => (
         <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
@@ -18,8 +29,8 @@ function Todo({todos, completeTodo}) {
             </div>
 
             <div className='icons'>
-                <RiCloseCricleLine />
-                <TiEdit />
+                <RiCloseCricleLine onClick={() => removeTodo(todo.id)} className='delete-icon' />
+                <TiEdit onClick={() => setEdit({ id: todo.id, value: todo.text })} className='edit-icon' />
             </div>
         </div>
     ))
